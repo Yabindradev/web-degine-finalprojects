905962b5baaebb4e0a919713e114f700a27cb3e2
yabindra <yabindra2057@gmail.com>
2023-01-23 09:43:04 +0900

    edit mobile views

--- src/index.js
+++ src/index.js
@@ -117,6 +117,26 @@ customElements.define('global-navibar', Globlenavibar)
 
 
 
+// scrools section
+
+        let mybutton = document.getElementById("myBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () { scrollFunction() };
+
+        function scrollFunction() {
+            if (document.body.scrollTop >5 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
 
 
 
